dc5c0997fadfbcc82aec23d7d5529d26df4cc598
NorVirae <norbertmbafrank@gmail.com>
2026-02-23 12:33:41 +0100

    Testing out numpy exponential function when working with sigmoid functions

---
+++ supervised/binary_classification/linear_reg_bad_classifier.py
@@ -0,0 +1,7 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x_train = np.array([0.0, 1, 2, 3, 4, 5])
+# y_train = np.array([0, 0, 0, 1, 1, 1])
+# x_train2 = np.array([[0.5, 1.5], [1, 1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+# y_train2 = np.array([0, 0, 0, 1, 1, 1])
---
+++ supervised/binary_classification/logistic_regression.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# checkout nupy exponential e^z
+array_el = np.arange(-10, 10)
+array_exp = np.exp(array_el)
+
+# print(f"Array elements : {array_el} \nArray Exponential: {array_exp}")
+
+# also does for single numbers
+single_el = 0
+single_exp = np.exp(single_el)
+print(f" Single Element: {single_el} \n Exponential Single Element: {single_exp}")
